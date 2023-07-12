5007e5ca7725fe61f9a8cb420bea97df5e420a5d
Isobel Ibironke <isobel.ibironke@softwire.com>
2023-07-12 16:08:50 +0100

    sidebar buttons

--- energy-board/src/App.tsx
+++ energy-board/src/App.tsx
@@ -1,27 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { getFuelTypes } from './repository/repository';
 import { FuelTypeList } from './components/FuelTypeList';
-
+import { useState } from 'react';
 
 
 export default function App() {
-
+  const [displayBoxContents, setDisplayBoxContents] = useState(<p>Welcome to the Elexon EnergyBoard!</p>);
 
   return (
     <div className="App">
       <h1>Elexon EnergyBoard</h1>
 
       <div className="sidenav">
-
         
+        <FuelTypeList/> <br></br>
+        
+        <button onClick={() => setDisplayBoxContents(<p>Interconnector Flows BarChart</p>)}>Interconnector Flows</button>
 
-        <FuelTypeList/>
-
-
-        <a href="#">Fuel Types</a>
       </div>
+
+      {displayBoxContents}
+
     </div>
   );
 }
