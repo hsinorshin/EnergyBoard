912c2e90c7a293b3da493b5372e619fb6cbf1867
Isobel Ibironke <isobel.ibironke@softwire.com>
2023-07-12 15:22:41 +0100

    create getInterFlowsTypes function

--- energy-board/src/repository/repository.ts
+++ energy-board/src/repository/repository.ts
@@ -15,4 +15,13 @@
    
 }
     
-  
+export function getInterFlowsTypes(){
+    fetch('https://data.dev.elexon.co.uk/bmrs/api/v1/generation/outturn/halfHourlyInterconnector?settlementDateFrom=2023-07-11&settlementDateTo=2023-07-11&settlementPeriod=1&format=json')
+    .then((res) => res.json())
+            .then((data) => {
+            console.log(data);
+            })
+            .catch((err) => {
+            console.log(err.message);
+            });
+    }
