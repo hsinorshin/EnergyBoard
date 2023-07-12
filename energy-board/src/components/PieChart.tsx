30dd49abed1ca238fdb575ff28c2260885b3bda4
Hsin Low <Hsin.Low@softwire.com>
2023-07-12 16:13:40 +0100

    working on piechart

--- energy-board/src/components/PieChart.tsx
+++ energy-board/src/components/PieChart.tsx
@@ -1,3 +1,14 @@
 import { useEffect, useState } from "react";
 import { PieChart, Pie } from "recharts";
+import { FuelProfile } from "../models/FuelProfileModel";
 
+const App= (datas: Array<FuelProfile>, field1: string, field2: string)=> {
+    datas.map(data => 
+    const chartdata =[
+        {name: data.field1, value: data.field2},
+        
+    ]
+    
+    )
+    
+}
