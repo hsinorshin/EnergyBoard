f46fce858e7ededd35eb0f44e4ef55ccae5f7d64
Isobel Ibironke <isobel.ibironke@softwire.com>
2023-07-13 12:35:16 +0100

    more formatting

--- energy-board/src/App.tsx
+++ energy-board/src/App.tsx
@@ -3,36 +3,37 @@ import { useEffect, useState } from 'react';
 import { interFlowsToBarChartData } from './mappers/interFlowsToBarChartData';
 import { BarChart } from './components/BarChart';
 import { FuelTypeList } from './components/FuelTypeList';
+import image from "./assets/elexonLogo.png";
 
 export default function App() {
-  const [displayBoxContents, setDisplayBoxContents] = useState(<p>Welcome to the Elexon EnergyBoard!</p>);
+  const [displayBoxContents, setDisplayBoxContents] = useState(homeDisplay);
 
   const interFlowBCD = interFlowsToBarChartData();
 
   return (
     <div className="App">
-      <h1>Elexon EnergyBoard</h1>
+
+      <div className="displayBox">
+        <h1>Elexon EnergyBoard</h1>
+        
+        {displayBoxContents}
+      </div>
 
       <div className="sidenav">
+        <button className="sidenavbtn" onClick={() => setDisplayBoxContents(homeDisplay)}>Home</button> <br></br>
         
-                
-        <button onClick={() => setDisplayBoxContents(<BarChart bcd={interFlowBCD}/>)}>Interconnector Flows</button> <br></br>
+        <button className="sidenavbtn" onClick={() => setDisplayBoxContents(<BarChart bcd={interFlowBCD}/>)}>Interconnector Flows</button> <br></br>
 
-        <button onClick={() => FuelTypeList}>Fuel Type ** </button> <br></br>        
+        <button className="sidenavbtn" onClick={() => FuelTypeList}>Fuel Type ** </button> <br></br>        
 
         {/* <FuelTypeList/> */}
 
       </div>
 
-      <div>
-        {displayBoxContents}
-      </div>
-
-
     </div>
   );
 }
 
-function GetFuelType(){
-  return null;
-}
+const homeDisplay = <h3>Welcome to the Elexon EnergyBoard! <br></br>  
+                       <img src={image} alt="Elexon Logo" width="700" height="400"></img> 
+                    </h3> 
