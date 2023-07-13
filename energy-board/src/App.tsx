9d90da5da124024667cbfe27e61bfb0086b09298
Hsin Low <Hsin.Low@softwire.com>
2023-07-13 11:48:07 +0100

    initial refactoring

--- energy-board/src/App.tsx
+++ energy-board/src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css';
 import { FuelTypeList } from './components/FuelTypeList';
 import { useState } from 'react';
+import { getFuelTypes } from './repository/repository';
+import { FuelProfile } from './models/FuelProfileModel';
+import { FuelTypePieChart } from './components/PieChart';
 
 
 export default function App() {
+
   const [displayBoxContents, setDisplayBoxContents] = useState(<p>Welcome to the Elexon EnergyBoard!</p>);
 
   return (
@@ -15,11 +19,7 @@ export default function App() {
         <button onClick={() => setDisplayBoxContents(<p>Fuel Generation Pie Chart</p>)}>Fuel Generation</button> <br></br>
         
 
-        <button onClick={() => FuelTypeList}>Fuel Type ** </button> <br></br>
-
-        
-
-        {/* <FuelTypeList/> */}
+        <button onClick={() => DisplayFuelTypes}>Fuel Type</button> <br></br>
 
 
       </div>
@@ -30,6 +30,48 @@ export default function App() {
   );
 }
 
-function GetFuelType(){
-  return null;
+const DisplayFuelTypes =() =>{
+  
+  const [data, setData] = useState([]);
+  
+  getFuelTypes()
+  .then((res) => res.json())
+        .then((actualData) => {
+          console.log(actualData);
+          setData(actualData)
+          console.log(data);
+          console.log("CONGRATULATIONS");
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+
+  
+  return (<>
+  
+  <div className="FuelType">
+    <tr>
+      <td>Fuel Type</td>
+      <td>Current Usage</td>
+      <td>Current Percentage</td>
+    </tr>
+
+    {data.length>0 && data.map(
+      (item: FuelProfile, index) =>(
+        
+        <tr key={index}>
+            <td>{item.fuelType}</td>
+            <td>{item.currentUsage}</td>
+            <td>{item.currentPercentage}</td>
+          </tr>
+      )
+    )}
+      
+      {FuelTypePieChart(data)}
+    
+      
+  </div>
+    
+  </>)
+  
 }
