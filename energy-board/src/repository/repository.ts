7f1da057ce1d828af733f67f5eca21db9a2d7a49
Isobel Ibironke <isobel.ibironke@softwire.com>
2023-07-13 15:42:41 +0100

    time stuff

--- energy-board/src/repository/repository.ts
+++ energy-board/src/repository/repository.ts
@@ -1,8 +1,11 @@
+import { getTodayDate } from "../helpers/timeFormatter";
+
 export function getFuelTypes(){
    return fetch('https://data.dev.elexon.co.uk/bmrs/api/v1/generation/outturn/FUELINSTHHCUR')
 }
     
 export function getInterFlows() {
-   return fetch('https://data.dev.elexon.co.uk/bmrs/api/v1/generation/outturn/halfHourlyInterconnector?settlementDateFrom=2023-07-11&settlementDateTo=2023-07-11&settlementPeriod=1&format=json');
+   const today = getTodayDate();
+   return fetch('https://data.dev.elexon.co.uk/bmrs/api/v1/generation/outturn/halfHourlyInterconnector?settlementDateFrom=' + today + '&settlementDateTo=' + today + '&format=json');
 }
 
