2ad9900debe7b61e872d94a03f2adc6afe62acce
Hsin Low <Hsin.Low@softwire.com>
2023-07-13 16:08:39 +0100

    refactoring

--- energy-board/src/App.tsx
+++ energy-board/src/App.tsx
@@ -2,20 +2,25 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import { interFlowsToBarChartData } from './mappers/interFlowsToBarChartData';
 import { BarChart } from './components/BarChart';
-import { FuelTypeList } from './components/FuelTypeList';
 import elexonLogo from "./assets/energyTypes.png";
 import { getCurrentTime } from './helpers/timeFormatter';
 import { emptyBCD } from './models/BarChartData';
+import { PieApp } from './components/PieChart';
+import { FuelTypeToPieChartData } from './mappers/fuelTypeToPieChartData';
+import { getFuelTypes } from './repository/repository';
 
 export default function App() {
   const [displayBoxContents, setDisplayBoxContents] = useState(homeDisplay);
   const [lastUpdated, setLastUpdated] = useState(getCurrentTime());
   const [interFlowBCD, setInterFlowBCD] = useState(structuredClone(emptyBCD));
+  
 
   useEffect(() => { interFlowsToBarChartData().then(bcd => setInterFlowBCD(bcd));
                     const interval = setInterval(() => {interFlowsToBarChartData().then(bcd => setInterFlowBCD(bcd)); setLastUpdated(getCurrentTime());}, 300000);
                     return () => clearInterval(interval);  
                   }, []);
+
+  
 
   return (
     <div className="App">
@@ -33,7 +38,7 @@ export default function App() {
         
         <button className="sidenavbtn" onClick={() => setDisplayBoxContents(<BarChart bcd={interFlowBCD}/>)}>Interconnector Flows</button> <br></br>
 
-        <button className="sidenavbtn" onClick={() => FuelTypeList}>Fuel Type ** </button> <br></br>        
+        <button className="sidenavbtn" onClick={() => setDisplayBoxContents(<PieApp pcd={FuelTypeToPieChartData(getFuelTypes())}/>)}>Fuel Type ** </button> <br></br>        
       </div>
     </div>
   );
