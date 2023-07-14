34ae4a9597f833922194dceb3d65a9c08b152747
Isobel Ibironke <isobel.ibironke@softwire.com>
2023-07-14 10:35:53 +0100

    time to next update

--- energy-board/src/App.tsx
+++ energy-board/src/App.tsx
@@ -3,28 +3,33 @@ import { useEffect, useState } from 'react';
 import { interFlowsToBarChartData } from './mappers/interFlowsToBarChartData';
 import { BarChart } from './components/BarChart';
 import elexonLogo from "./assets/energyTypes.png";
-import { getCurrentTime } from './helpers/timeFormatter';
 import { emptyBCD } from './models/BarChartData';
 import { PieApp } from './components/PieChart';
 import { FuelTypeToPieChartData } from './mappers/fuelTypeToPieChartData';
 import { getFuelTypes } from './repository/repository';
 import { emptyFuelProfileArray } from './models/FuelProfileModel';
 import { FuelTypeDisplayTable } from './components/DisplayTable';
+import { getCurrentTime, getTimeToNextUpdate, updateInterval } from './helpers/dateTimeFuncs';
 
 export default function App() {
   const [displayBoxContents, setDisplayBoxContents] = useState(homeDisplay);
+
   const [lastUpdated, setLastUpdated] = useState(getCurrentTime());
+  const [timeToNextUpdate, setTimeToNextUpdate] = useState(getTimeToNextUpdate(lastUpdated));
+
   const [interFlowBCD, setInterFlowBCD] = useState(structuredClone(emptyBCD));
   const [fuelTypePCD, setFuelTypePCD] = useState(emptyFuelProfileArray);
   
 
   useEffect(() => { interFlowsToBarChartData().then(bcd => setInterFlowBCD(bcd));
-                    const interval = setInterval(() => {interFlowsToBarChartData().then(bcd => setInterFlowBCD(bcd)); setLastUpdated(getCurrentTime());}, 300000);
+                    getFuelTypes().then(pcd =>setFuelTypePCD(pcd));
+                    const interval = setInterval(() => {interFlowsToBarChartData().then(bcd => setInterFlowBCD(bcd)); 
+                                                        setLastUpdated(getCurrentTime());
+                                                        getFuelTypes().then(pcd => setFuelTypePCD(pcd));}, updateInterval);
                     return () => clearInterval(interval);  
                   }, []);
-  useEffect(()=> {getFuelTypes().then(pcd =>setFuelTypePCD(pcd));
-                  const interval= setInterval(() => {getFuelTypes().then(pcd => setFuelTypePCD(pcd)); setLastUpdated(getCurrentTime());}, 300000);
-  }, []);
+
+  useEffect(() => {const timer = setTimeout(() => {setTimeToNextUpdate(getTimeToNextUpdate(lastUpdated));}, 1000);});
 
   return (
     <div className="App">
@@ -34,9 +39,9 @@ export default function App() {
         
         {displayBoxContents} <br></br>
 
-        
-        <p>Last Updated: {lastUpdated}</p>
       </div>
+
+      <div className='footer'> Last Updated: {lastUpdated} &emsp; &emsp;  &emsp; Time to Next Update: {timeToNextUpdate}</div>
 
       <div className="sidenav">
         <button className="sidenavbtn" onClick={() => setDisplayBoxContents(homeDisplay)}>Home</button> <br></br>
--- energy-board/src/helpers/dateTimeFuncs.ts
+++ energy-board/src/helpers/dateTimeFuncs.ts
@@ -10,3 +10,25 @@ export function getTodayDate() {
     const today: Date = new Date();
     return today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + (today.getDate() < 10 ? '0' : '') + today.getDate();
 }
+
+export function getTimeToNextUpdate(lastUpdateTime : string) {
+    const lastUpdate = new Date("01/01/2021 " + lastUpdateTime);
+    const now = new Date("01/01/2021 " + getCurrentTime())
+    const timeToNextUpdate = lastUpdate.getTime() + updateInterval - now.getTime();
+
+    const SEC = 1000;
+    const MIN = 60 * SEC;
+    const HRS = 60 * MIN;
+
+    let hrs = Math.floor(timeToNextUpdate/HRS);
+    let min = Math.floor((timeToNextUpdate%HRS)/MIN);
+    let sec = Math.floor((timeToNextUpdate%MIN)/SEC);
+
+    hrs = hrs < 0 ? 0 : hrs;
+    min = min < 0 ? 0 : min;
+    sec = sec < 0 ? 0 : sec;
+
+    return `${(hrs < 10 ? '0' : '') + hrs}:${(min < 10 ? '0' : '') + min}:${(sec < 10 ? '0' : '') + sec}`
+}
+
+export const updateInterval = 300000;
